2acdeb6c56f3a47734538c2f64b5fabf71fd2af5
Ayan Sujon <ayansujonbd@gmail.com>
2025-10-28 23:01:02 +0600

    successfully connected to MongoDB

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
+// Claster0 user and password error
 // simpleDBUser
 // Je92iM98b7AiXa8p
+
+
+// Claster1 User and Password 
+
 // const uri = "mongodb+srv://simpleDBUser:<db_password>@cluster0.tachgq7.mongodb.net/?appName=Cluster0";
 const uri = "mongodb+srv://simpleDBUser:Je92iM98b7AiXa8p@cluster0.tachgq7.mongodb.net/?appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,7 +39,7 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
     finally{
-
+      // await client.close();
     }
 }
 run().catch(console.dir)
